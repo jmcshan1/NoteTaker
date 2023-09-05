46141c57b6182c608600d1d42739e300d6bba586
jmcshan1 <jmcshan01@gmail.com>
2023-09-04 20:39:04 -0500

    cleared database file

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 // Creates an instance of express in the app variable
 const app = express();
 
-// 
 const PORT = process.env.PORT || 3001;
 const api = require('./routes/index');
 
